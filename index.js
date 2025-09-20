3ff016a6e4d023e4c4842b171b5d12c5961c7e36
Ann Grace Evans <agevans789@gmail.com>
2025-09-20 10:37:43 -0400

    steps 1-4

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-
-
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+  };
+  // loop through args to isolate each array
+  for (const arg of args) {
+    if (Array.isArray(arg)) {
+      // add the spread of args to the users array 
+      combinedObject.users.push(...arg);
+    }
+  };
+  // add attribute to combinedObject called merge_date
+  
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
